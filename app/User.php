9ffae34d6ded7c98b8eeeaa2b5eb4ba90147ffbc
Mathias Hansen <me@codemonkey.io>
2019-11-14 21:18:23 -0500

    Started data modelling and tailwind-ing

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -25,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'code',
     ];
 
     /**
@@ -34,6 +33,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone', 15)->unique();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('code')->unique()->index();
             $table->timestamps();
         });
     }
