8ae2e06ac71c8b10414439c550ea02c40bbe3a7e
Mathias Hansen <me@codemonkey.io>
2019-11-16 15:50:43 -0500

    Better colors for terminal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+        colors: {
+            'red-status': '#f36a5e',
+            'yellow-status': '#fabd4d',
+            'green-status': '#58c454'
+        }
+    }
   },
   variants: {},
   plugins: []
