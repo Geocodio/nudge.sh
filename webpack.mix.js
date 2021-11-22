c5bd518d54142edac41a1bdcaa36443a4019c4eb
Mathias Hansen <me@codemonkey.io>
2021-11-22 11:02:32 +0100

    chore: Updated dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,8 +15,9 @@ require('laravel-mix-purgecss');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+   .postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+  ]);
 
 if (mix.inProduction()) {
   mix
