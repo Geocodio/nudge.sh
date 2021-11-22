c0d2f6faa27d03823269fc6aabcc52d6eacb30bf
Shift <shift@laravelshift.com>
2021-11-22 09:55:25 +0000

    Shift cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\OutputController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OutputController;
 use Illuminate\Support\Facades\Route;
 
 /*
